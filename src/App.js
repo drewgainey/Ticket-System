25c613dfa97dd44aa0fd609261f94f7d4561ded6
Drew Gainey <drewgainey@gmail.com>
2022-05-26 16:07:36 -0400

    updated palete

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import LogInSignUp from "./pages/LogInSignUp";
 import SearchTickets from "./pages/SearchTickets";
 import { AuthProvider } from "./contexts/AuthContext";
 import PrivateRoute from "./components/PrivateRoute";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: { main: "#000000" },
+    },
+  });
   return (
     <AuthProvider>
-      <div className="App">
-        <Router>
-          <Route exact path="/" component={LogInSignUp} />
-          <PrivateRoute path="/new" component={SupportTicket} />
-          <PrivateRoute path="/home" component={Home} />
-          <PrivateRoute path="/search" component={SearchTickets} />
-        </Router>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Route exact path="/" component={LogInSignUp} />
+            <PrivateRoute path="/new" component={SupportTicket} />
+            <PrivateRoute path="/home" component={Home} />
+            <PrivateRoute path="/search" component={SearchTickets} />
+          </Router>
+        </div>
+      </ThemeProvider>
     </AuthProvider>
   );
 }
